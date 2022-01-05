84acfa96487616f40cb320223658060c44216be6
Aliaksandr <al_brain8@tut.by>
2022-01-05 13:27:39 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        n=n-n*2
+        n=n.toString().split('').reverse();
+   n=Number(n.join(''));
+   return n
+  }else{
+        n=n.toString().split('').reverse();
+   n=Number(n.join(''));
+   return n
+  }
 }
+
